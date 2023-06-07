493762be40d611025aa747822a8390c638a7923d
Roy-de <roymuuri28@gmail.com>
2023-06-07 12:42:17 +0300

    Added Security

--- src/main/java/com/example/login/Controller/RegistrationController.java
+++ src/main/java/com/example/login/Controller/RegistrationController.java
@@ -3,11 +3,20 @@ package com.example.login.Controller;
 import com.example.login.Entity.Data.UserData;
 import com.example.login.Service.UserService;
 import lombok.SneakyThrows;
-import org.springframework.ui.Model;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasAuthority;
+
 @RestController
-@RequestMapping("/registration")
+@RequestMapping("/")
+@EnableReactiveMethodSecurity
+@EnableMethodSecurity
 public class RegistrationController {
     UserService userService;
 
@@ -21,12 +30,29 @@ public class RegistrationController {
     }
     @GetMapping
     public String showRegistrationForm(){
-        return "registration";
+        return "/login";
     }
     @SneakyThrows
-    @PostMapping
+    @PostMapping("/signup")
     public String registerUser(@ModelAttribute("user") UserData userData) {
         userService.Register(userData);
-        return "redirect:/registration?success";
+        return "redirect:/signup?success";
+    }
+    @GetMapping("/login")
+    public String showLoginForm(@ModelAttribute("user") UserData userData){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null || authentication instanceof AnonymousAuthenticationToken){
+            return "login";
+        }
+        return "redirect:/login?success";
+    }
+    @GetMapping("/home")
+    public String homepage(){
+        return "Welcome home m**fucker";
+    }
+    @GetMapping("/Dashboard")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String dashboard(){
+        return "dashboard";
     }
 }
--- src/main/java/com/example/login/config/LoginConfig.java
+++ src/main/java/com/example/login/config/LoginConfig.java
@@ -2,37 +2,37 @@ package com.example.login.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
 public class LoginConfig {
     @Bean
-    @Order(100)
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
-                .securityMatcher("/user")
-                .authorizeHttpRequests((authConfig)-> authConfig
-                        .requestMatchers("/user/**").hasAnyAuthority("ROLE_USER")
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
-
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
+        UserDetails user = User.withUsername("Admin")
+                .password(passwordEncoder.encode("password1"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http.authorizeHttpRequests((authorize)-> authorize
+                .requestMatchers("/signup","/login","/home").permitAll())
+                .formLogin((httpSecurityFormLoginConfigurer) -> httpSecurityFormLoginConfigurer.loginPage("/login").permitAll());
         return http.build();
     }
     @Bean
-    @Order(101)
-    public SecurityFilterChain securityFilterChain1(HttpSecurity http) throws Exception{
-        http
-                .securityMatcher("/admin")
-                .authorizeHttpRequests((authConfig)-> authConfig
-                        .requestMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
-
-        return http.build();
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
